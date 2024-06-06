5f8a4e808127413e23cec0b723576a5918cc9074
Dhiman Family <dhiman@dhimans-mini.home>
2024-06-06 17:15:29 -0400

    initial code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=sunny
+export POSTGRES_PASSWORD=9855650316
+export POSTGRES_HOST=cdr.cbpr5hqtyguu.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-dev-project
+export AWS_REGION=us-east-1
+export AWS_PROFILE=myAdmin
+export JWT_SECRET=testing
+export URL=http://localhost:8100
