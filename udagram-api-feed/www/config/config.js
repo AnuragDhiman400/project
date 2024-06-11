fe091ddb68d0d5a1540673014a7625d9253ed075
anuragdhiman400 <anuragdhiman400@gmail.com>
2024-06-11 18:51:16 -0400

    Updated www

---
+++ udagram-api-feed/www/config/config.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    'username': "postgres",
+    'password': "9855650316",
+    'database': "postgres",
+    'host': "rdsdb.cbpr5hqtyguu.us-east-1.rds.amazonaws.com",
+    'dialect': 'postgres',
+    'aws_region': "us-east-1",
+    'aws_profile': "myAdmin",
+    'aws_media_bucket': "udagram-dev-project",
+    'url': "http://localhost:8100",
+    'jwt': {
+        'secret': "testing",
+    },
+};
+//# sourceMappingURL=config.js.map
