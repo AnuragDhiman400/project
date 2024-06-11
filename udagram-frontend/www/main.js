6cf06ee02e84ac91de0f8962ddaf1eaa5d03ddd8
anuragdhiman400 <anuragdhiman400@gmail.com>
2024-06-11 19:38:07 -0400

    Updated backend URL

--- udagram-frontend/www/main.js
+++ udagram-frontend/www/main.js
@@ -1660,7 +1660,7 @@ __webpack_require__.r(__webpack_exports__);
 var environment = {
     production: false,
     appName: 'Udagram',
-    apiHost: 'http://localhost:8080/api/v0'
+    apiHost: 'http://af5df84bda6f144328332447900bbd0a-839864331.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 /*
  * For easier debugging in development mode, you can import the following file
